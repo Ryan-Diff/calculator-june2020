502b43c3cd36058485672eb940f65fb05db6697a
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-06-25 13:58:52 -0700

    Calc with changes

--- app.js
+++ app.js
@@ -25,3 +25,44 @@ boogerButton.addEventListener('click', () => {
     // update the textContent of the span
     boogerSpan.textContent = sum;
 });
+
+// Subtraction
+const subInput1 = document.getElementById("sub-input-1");
+const subInput2 = document.getElementById("sub-input-2");
+const subButton = document.getElementById("sub-button");
+const subSpan = document.getElementById("sub-result");
+
+subButton.addEventListener('click', () => {
+    const value1 = subInput1.value;
+    const value2 = subInput2.value;
+    const sub = Number(value1) - Number(value2);
+    subSpan.textContent = sub;
+});
+
+// Multiplication
+const multInput1 = document.getElementById("mult-input-1");
+const multInput2 = document.getElementById("mult-input-2");
+const multButton = document.getElementById("mult-button");
+const multspan = document.getElementById("mult-result");
+
+multButton.addEventListener("click", () => {
+    const value1 = multInput1.value;
+    const value2 = multInput2.value;
+    const mult = Number(value1) * Number(value2);
+    multspan.textContent = mult;
+});
+
+// Division
+const divInput1 = document.getElementById("div-input-1");
+const divInput2 = document.getElementById("div-input-2");
+const divButton = document.getElementById("div-button");
+const divspan = document.getElementById("div-result");
+
+divButton.addEventListener("click", () => {
+    const value1 = divInput1.value;
+    const value2 = divInput2.value;
+    const div = Number(value1) / Number(value2);
+    divspan.textContent = div;
+});
+
+
